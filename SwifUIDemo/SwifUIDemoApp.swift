53849a4126aa6a6f62871cccae078f511b0e8e8b
Rahul Sharma <rahul.sharma@dubswork.com>
2022-05-01 09:26:34 +0530

    Initial Commit

---
+++ SwifUIDemo/SwifUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIDemoApp.swift
+//  SwifUIDemo
+//
+//  Created by Headspire Technologies on 01/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
