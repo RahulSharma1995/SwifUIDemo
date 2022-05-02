a9d9629763a9380ff3ef14a9ea92803fc9fa34eb
Rahul Sharma <rahul.sharma@dubswork.com>
2022-05-02 10:39:29 +0530

    save data in userDefault

--- SwifUIDemo/ViewModels/ListViewModel.swift
+++ SwifUIDemo/ViewModels/ListViewModel.swift
@@ -8,19 +8,22 @@
 import Foundation
 
 class ListViewModel : ObservableObject {
-    @Published var items: [ItemModel] = []
-    
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     init(){
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isComplited: false),
-            ItemModel(title: "This is the secound!", isComplited: true),
-            ItemModel(title: "Third!", isComplited: false),
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet){
@@ -39,6 +42,12 @@ class ListViewModel : ObservableObject {
     func updateItem(item : ItemModel){
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateModel()
+        }
+    }
+    
+    func saveItems(){
+        if let encodedModel = try? JSONEncoder().encode(items){
+            UserDefaults.standard.set(encodedModel, forKey: itemsKey)
         }
     }
     
